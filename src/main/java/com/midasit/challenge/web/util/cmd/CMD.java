bf9acddea2c6bf0b4a93c913ba986ab42204bf80
psycho3081 <psycho3081@dgsw.hs.kr>
2018-05-26 14:42:44 +0900

    add CMD class

---
+++ src/main/java/com/midasit/challenge/web/util/cmd/CMD.java
@@ -0,0 +1,21 @@
+package com.midasit.challenge.web.util.cmd;
+
+public enum CMD {
+    INSERT, VIEW, DELETE, UPDATE, LIST;
+
+    public int code() {
+        return ordinal() + 101;
+    }
+
+    public static String description(CMD cmd) {
+        StringBuilder str = new StringBuilder("Failed to ");
+        switch (cmd) {
+            case INSERT: str.append("INSERT");break;
+            case VIEW: str.append("VIEW");break;
+            case DELETE: str.append("DELETE");break;
+            case UPDATE: str.append("UPDATE");break;
+            case LIST: str.append("LIST");break;
+        }
+        return str.toString();
+    }
+}
